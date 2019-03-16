9ce481fb96d600c53a101dfafbcc588cab123835
Saman Khamesian <saman.khamesian@gmail.com>
2019-03-16 23:10:48 +0330

    BFS algorithm added.

---
+++ Breadth-first search (BFS)/README.md
@@ -0,0 +1,11 @@
+# Breadth-first search (BFS)
+
+Breadth-first search (BFS) is an algorithm for traversing or searching tree or graph data structures.
+It starts at the tree root (or some arbitrary node of a graph) and explores the neighbor nodes first, before moving to the next level neighbors.
+
+#### Input parameters
+* `start` : Starting node (Source node)
+
+#### Results
+* `dist` : Array of minimum distances from source node to the others.
+* `parent` : Array of parents for the shortest path between source node and target node.
---
+++ Breadth-first search (BFS)/Source.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <queue>
+#include <climits>
+#include <vector>
+
+using namespace std;
+
+#define INF INT_MAX
+
+int n, q, v, w;
+
+vector<vector<int>> list;
+
+vector<bool> marked;
+
+vector<int> parent;
+
+vector<int> dist;
+
+queue<int> bfsq;
+
+void bfs(int start)
+{
+	for (int i = 0; i <= n ; i++)
+	{
+		dist[i] = INF;
+
+		parent[i] = -1;
+	}
+
+	marked[start] = true;
+
+	dist[start] = 0;
+
+	bfsq.push(start);
+
+	while (!bfsq.empty())
+	{
+		int temp = bfsq.front();
+
+		bfsq.pop();
+
+		for (int i = 0; i < list[temp].size(); i++)
+		{
+			if (!marked[list[temp][i]])
+			{
+				bfsq.push(list[temp][i]);
+
+				dist[list[temp][i]] = dist[temp] + 1;
+
+				marked[list[temp][i]] = true;
+
+				parent[list[temp][i]] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	cin >> n >> q;
+
+	list.resize(n + 1);
+
+	dist.resize(n + 1);
+
+	marked.resize(n + 1);
+
+	parent.resize(n + 1);
+
+	for (int i = 0; i < q; i++)
+	{
+		cin >> v >> w;
+
+		list[v].push_back(w);
+
+		list[w].push_back(v);
+	}
+
+	// Find all shortest paths from edge 1 to others
+
+	bfs(1);
+}
