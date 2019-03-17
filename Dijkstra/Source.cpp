0f733f21f292bbdee21461634f98352b404ecb95
Saman Khamesian <saman.khamesian@gmail.com>
2019-03-17 20:21:55 +0330

    Code style changed.

--- Dijkstra/Source.cpp
+++ Dijkstra/Source.cpp
@@ -7,12 +7,10 @@
 using namespace std;
 
 #define INF INT_MAX
-
 #define SIZE 100010
 
 int dis[SIZE];
-
-vector<pair<int, int>> list[SIZE];
+vector< pair<int, int> > list[SIZE];
 
 class comparator
 {
@@ -32,33 +30,25 @@ void dijkstra(int start)
 	}
 
 	dis[start] = 0;
-
 	priority_queue<pair<int, int>, vector<pair<int, int>>, comparator> pq;
-
 	pq.push(make_pair(start, dis[start]));
 
 	while (!pq.empty())
 	{
 		pair<int, int> head = pq.top();
-		
 		pq.pop();
-
 		int headVertex = head.first;
-
 		int headWeight = head.second;
-
 		int childVertex, childWeight;
 
 		for (int i = 0; i < list[headVertex].size(); i++)
 		{
 			childVertex = list[headVertex][i].first;
-
 			childWeight = list[headVertex][i].second;
 
 			if (childWeight + headWeight < dis[childVertex])
 			{
 				dis[childVertex] = childWeight + headWeight;
-
 				pq.push(make_pair(childVertex , dis[childVertex]));
 			}
 		}
@@ -68,15 +58,12 @@ void dijkstra(int start)
 int main()
 {
 	int n, m, x, y, w;
-	
 	cin >> n >> m;
 
 	for (int i = 0; i < m; i++)
 	{
 		cin >> x >> y >> w;
-
 		list[x].push_back(make_pair(y, w));
-
 		list[y].push_back(make_pair(x, w));
 	}
 
