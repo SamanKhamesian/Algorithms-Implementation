b40ec2482cf157924be72edba22f7464e07445ec
Saman Khamesian <saman.khamesian@gmail.com>
2019-03-17 20:19:44 +0330

    Code style changed.

--- Breadth-first search (BFS)/Source.cpp
+++ Breadth-first search (BFS)/Source.cpp
@@ -8,15 +8,9 @@ using namespace std;
 #define INF INT_MAX
 
 int n, m, v, w;
-
-vector<vector<int>> list;
-
+vector< vector<int> > list;
 vector<bool> marked;
-
-vector<int> parent;
-
-vector<int> dist;
-
+vector<int> parent, dist;
 queue<int> bfsq;
 
 void bfs(int start)
@@ -24,20 +18,16 @@ void bfs(int start)
 	for (int i = 0; i <= n ; i++)
 	{
 		dist[i] = INF;
-
 		parent[i] = -1;
 	}
 
 	marked[start] = true;
-
 	dist[start] = 0;
-
 	bfsq.push(start);
 
 	while (!bfsq.empty())
 	{
 		int temp = bfsq.front();
-
 		bfsq.pop();
 
 		for (int i = 0; i < list[temp].size(); i++)
@@ -45,11 +35,8 @@ void bfs(int start)
 			if (!marked[list[temp][i]])
 			{
 				bfsq.push(list[temp][i]);
-
 				dist[list[temp][i]] = dist[temp] + 1;
-
 				marked[list[temp][i]] = true;
-
 				parent[list[temp][i]] = temp;
 			}
 		}
@@ -59,21 +46,15 @@ void bfs(int start)
 int main()
 {
 	cin >> n >> m;
-
 	list.resize(n + 1);
-
 	dist.resize(n + 1);
-
 	marked.resize(n + 1);
-
 	parent.resize(n + 1);
 
 	for (int i = 0; i < m; i++)
 	{
 		cin >> v >> w;
-
 		list[v].push_back(w);
-
 		list[w].push_back(v);
 	}
 
